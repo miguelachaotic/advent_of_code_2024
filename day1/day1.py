e82f4adf70a9fb05d6b23d97b7385ed2c5c19464
= <=>
2024-12-02 08:13:53 +0100

    modified day1.py for better output

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,4 @@
 data = [*map(int, open('input.txt').read().split())]
 A, B = sorted(data[0::2]), sorted(data[1::2])
-print(sum(map(lambda a, b: abs(a-b), A, B)),
-      sum(map(lambda a: a * B.count(a), A)))
+print("Part 1:", sum(map(lambda a, b: abs(a-b), A, B)),
+      "\nPart 2:", sum(map(lambda a: a * B.count(a), A)))
